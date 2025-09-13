65f51fc6517999a55f27eb19503aef989e68f0d2
Sanika <sanikasurade2021@gmail.com>
2025-09-13 10:41:01 +0530

    forgotpass routes added

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -7,9 +7,9 @@ const authRouter=express.Router()
 authRouter.post("/signup",signUp)//using post method because we are taking something from req.body
 authRouter.post("/signin",signIn)
 authRouter.get("/signout", signOut); 
-authRouter.get("/sendOtp", sendOtp); 
-authRouter.get("/verifyOtp", verifyOtp); 
-authRouter.get("/resetPassword", resetPassword); 
+authRouter.post("/sendOtp", sendOtp); 
+authRouter.post("/verifyOtp", verifyOtp); 
+authRouter.post("/resetPassword", resetPassword); 
 
 
 
