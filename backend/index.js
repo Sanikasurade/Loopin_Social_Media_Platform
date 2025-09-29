470dc16de0c4fd7fa430c1847e95b9ff70bef59a
Sanika <sanikasurade2021@gmail.com>
2025-09-29 18:47:33 +0530

    loop and post routes added

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
+import postRouter from "./routes/post.routes.js";
+import loopRouter from "./routes/loop.routes.js";
 dotenv.config();
 
 const app = express();
@@ -16,11 +18,14 @@ app.use(
     credentials: true, //
   })
 );
-app.use(express.json()); //it will tell the routes that what ever data is coming from frontend is in json format so that we will not  get the error of undefined
+app.use(express.json()); //it will tell the routes that what every data is coming from frontend is in json format so that we will not  get the error of undefined
 app.use(cookieParser()); //
 
 app.use("/api/auth", authRouter); //it will add the api/auth in front of all routes that we have created from authRouter
 app.use("/api/user", userRouter);
+app.use("/api/post", postRouter);
+app.use("/api/loop", loopRouter);
+
 
 app.listen(port, () => {
   connectDb();
