0bb6c3e5179df0603a361e46bb8457c360269265
Sanika <sanikasurade2021@gmail.com>
2025-09-29 18:54:01 +0530

    upload file works on nav bar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -14,20 +14,37 @@ function Nav() {
         const navigate=useNavigate();
         const {userData}=useSelector(state=>state.user)
     return (
-        <div className='w-[90%] lg:w-[40%] h-[50px] bg-black 
+      <div
+        className="w-[90%] lg:w-[40%] h-[50px] bg-black 
         flex justify-around items-center fixed bottom-[20px]
-         rounded-full shadow-2xl shadow-[#000000] z-[100]'>
-            {/* Navigation content goes here */}
-           <div onClick={()=>navigate("/")}> <FaHome className='text-white w-[25px] h-[25px]'/></div>
-           <div><FaSearch className='text-white w-[25px] h-[25px]' /></div>
-            <div><FaSquarePlus className='text-white w-[25px] h-[25px]' /></div>
-             {/* Profile image */}
-                                <div className='w-[30px] h-[30px] border-2 border-black
-                                 rounded-full cursor-pointer overflow-hidden' onClick={()=>navigate(`/profile/${userData.userName}`)}> 
-                                    <img src={ userData?.profileImage || dp1} alt="" className='w-full object-cover'/>
-                                    {/* Show user's profile image, else fallback to default dp */}
-                                </div>
+         rounded-full shadow-2xl shadow-[#000000] z-[100]"
+      >
+        {/* Navigation content goes here */}
+        <div onClick={() => navigate("/")}>
+          {" "}
+          <FaHome className="text-white w-[25px] h-[25px]" />
         </div>
+        <div>
+          <FaSearch className="text-white w-[25px] h-[25px] cursor-pointer" />
+        </div>
+        <div onClick={() => navigate("/upload")}>
+          <FaSquarePlus className="text-white w-[25px] h-[25px] cursor-pointer" />
+        </div>
+        {/* Profile image */}
+        <div
+          className="w-[30px] h-[30px] border-2 border-black
+        rounded-full cursor-pointer overflow-hidden"
+          onClick={() => navigate(`/profile/${userData.userName}`)}
+        >
+          <img
+            src={userData?.profileImage || dp1}
+            alt=""
+            className="w-full object-cover"
+          />
+          {/* Show user's profile image, else fallback to default dp */}
+        </div>
+        
+      </div>
     );
 }
 
